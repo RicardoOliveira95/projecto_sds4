7941ac82e0f58b8d84b38847378d1f027c65afa5
Ricardo Oliveira <rica11h18@gmail.com>
2021-09-13 01:16:50 +0100

    minor changes

--- frontend/src/components/dataTable/index.tsx
+++ frontend/src/components/dataTable/index.tsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import {useState,useEffect} from 'react';
+import {SalePage} from 'types/sale';
+import axios from 'axios';
+import {BASE_URL} from 'utils/requests';
+import {formatLocalDate} from 'utils/format';
 
 function DataTable(){
+
+  const [page,setPage]=useState<SalePage>({
+  		first: true,
+  		last: true,
+  		number: 0,
+  		totalElements: 0,
+  		totalPages:0,
+  });
+
+  useEffect(()=>{
+  	axios.get(BASE_URL+'/sales?page=0&size=2&sort=date,desc').then(res=>{
+  		setPage(res.data);
+  	});
+  },[]);
+
   return(
-    <div className="table-responsive">
+    <div id="tab" className="table-responsive">
     <table className="table table-striped table-sm">
         <thead>
             <tr>
@@ -14,16 +34,21 @@ function DataTable(){
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+            {page.content?.map(item=>{
+            console.log(item);
+            <tr key={item.id}>
+                //<td>{formatLocalDate(item.date,"yyyy-MM-dd")}</td>
+                tab=document.getElementById(tab);
+                tab.innerHTML=item.id
+                <td>{item.date}</td>
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
+            </tr>})}
         </tbody>
     </table>
 </div>);
 }
 
-export default NavBar;
+export default DataTable;
